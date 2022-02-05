b01a8dc5bd639aed8660087e6be85aa8a6bf5d85
joeneedum <joeneedum@gmail.com>
2022-02-04 19:53:25 -0500

    commit

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,9 +11,12 @@ export default class JokeList extends Component {
     };
     constructor(props) {
         super(props);
-        this.state = { jokes: [] };
+        this.state = { jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]' ) };
     }
     async componentDidMount() {
+        if(this.state.jokes.length === 0) this.getJokes();
+    }
+    async getJokes(){
         let jokes = [];
         while(jokes.length < this.props.numJokesToGet) {
             let res = await axios.get('https://icanhazdadjoke.com/', {
@@ -22,6 +25,7 @@ export default class JokeList extends Component {
             jokes.push( {id: uuidv4(), text: res.data.joke, votes: 0} )
         }
         this.setState({ jokes: jokes })
+        window.localStorage.setItem('jokes', JSON.stringify(jokes))
     }
     handleVote(id, delta) {
         this.setState(st => ({
